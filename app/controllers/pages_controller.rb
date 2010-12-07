c18ad23b236af0fa7f996102bc9fb1ea964854fa
Stephen B Silverberg <blairsilverberg@gmail.com>
2010-12-07 07:32:29 -0800

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def contact
+    @title = "Contact"
+  end
+  
+  def about
+    @title = "About" #instance variable whose primary purpose is to link actions and views -> automatically available in about.html.erb
   end
 
 end
